a64a3c7ce4a67667e7886628e82886e72d14767e
dan the man <82744745+wdantheman@users.noreply.github.com>
2023-06-01 10:12:16 -0600

    Update Class1.cs

--- Domain.UseCases/Class1.cs
+++ Domain.UseCases/Class1.cs
@@ -2,6 +2,9 @@
 {
     public class Class1
     {
+        //Ok, so, we need to add to the use cases the "autofill" with previous system entries, at least on the fucking doc
+        //also, at what extend do we want to expand the persistence comparation? 
+
 
     }
 }
