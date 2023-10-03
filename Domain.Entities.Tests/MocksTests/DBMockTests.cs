839f3dbbce49252b1bc4545b462d0efd7661abc1
DantheMan <wdaniel.ciencias1@gmail.com>
2023-10-03 16:15:12 +0000

    DBMock load refactoring

---
+++ Domain.Entities.Tests/MocksTests/DBMockTests.cs
@@ -0,0 +1,216 @@
+﻿using Domain.Entities.DataObjects.EntryComposite;
+using Domain.Entities.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Domain.Entities.DataObjects.DocumentComposite;
+
+namespace Domain.Entities.Tests.MocksTests
+{
+    public class DBMockTests
+    {
+        [Fact]
+        public void GetSections_ReturnsAllSections()
+        {
+            // Arrange
+            var dbMock = new DBMock(docNumber: 2, SectionsPerDoc: 3, entriesPerSection: 4);
+
+            // Act
+            var sections = dbMock.GetSections();
+
+            // Assert
+            Assert.Equal(6, sections.Count);
+        }
+        [Fact]
+        public void DictionaryLoadsCorrectly()
+        {
+            // Arrange
+            int docNUmber = 10;
+            int sectionsPerDoc = 10;
+            int Entries = 10;
+            // Act
+            DBMock sut = new DBMock(docNUmber, sectionsPerDoc, Entries);
+            // Assert
+            Assert.Equal(docNUmber, sut.Documents.Count());
+            Assert.Equal(sectionsPerDoc * docNUmber, sut.Sections.Count());
+            Assert.Equal(sectionsPerDoc * docNUmber * Entries, sut.Entries.Count());
+            Assert.Equal(sut.GetSectionComposite(1, 2).Title, sut.Sections[(1, 2)].Title);
+            // el rango de las secciones es i+10, el de las Entradas debe ser de i + 20 
+        }
+
+        [Fact]
+        public void GetSectionComposite_ValidIds_ReturnsSection()
+        {
+            // Arrange
+            var dbMock = new DBMock(docNumber: 3, SectionsPerDoc: 3, entriesPerSection: 4);
+
+            // Act
+            SectionComposite section = dbMock.GetSectionComposite(1, 2);
+
+            // Assert
+            Assert.NotNull(section);
+            Assert.Equal("section test2", section.Title);
+        }
+
+        [Fact]
+        public void GetSectionComposite_InvalidIds_ReturnsNull()
+        {
+            // Arrange
+            var dbMock = new DBMock(docNumber: 2, SectionsPerDoc: 3, entriesPerSection: 4);
+
+            // Act
+            var section = dbMock.GetSectionComposite(documentId: 5, sectionId: 2);
+
+            // Assert
+            Assert.Null(section);
+        }
+
+        [Fact]
+        public void ReadEntryinSection_ValidIds_ReturnsEntry()
+        {
+            // Arrange
+            var dbMock = new DBMock(docNumber: 2, SectionsPerDoc: 10, entriesPerSection: 4);
+            Entry temp = new Entry(12, "test");
+            dbMock.CreateEntryinSection(1,2,temp);
+            // Act
+            var entry = dbMock.ReadEntryinSection(documentId: 1, entryId: 12);
+
+            // Assert
+            Assert.NotNull(entry);
+            Assert.Equal(temp.Content, entry.Content);
+        }
+
+        [Fact]
+        public void ReadEntryinSection_InvalidIds_ReturnsNull()
+        {
+            // Arrange
+            var dbMock = new DBMock(docNumber: 2, SectionsPerDoc: 3, entriesPerSection: 4);
+
+            // Act
+            var entry = dbMock.ReadEntryinSection(documentId: 5, entryId: 2);
+
+            // Assert
+            Assert.Null(entry);
+        }
+
+        [Fact]
+        public void CreateEntryinSection_ValidData_EntryAdded()
+        {
+            // Arrange
+            var dbMock = new DBMock(docNumber: 2, SectionsPerDoc: 3, entriesPerSection: 4);
+            var newEntry = new Entry(999, "new entry content");
+
+            // Act
+            dbMock.CreateEntryinSection(documentId: 0, sectionId: 1, newEntry);
+            var entry = dbMock.ReadEntryinSection(documentId: 0, entryId: 999);
+
+            // Assert
+            Assert.Equal(newEntry, entry);
+        }
+
+        [Fact]
+        public void CreateEntryinSection_InvalidSection_ThrowsException()
+        {
+            // Arrange
+            var dbMock = new DBMock(docNumber: 2, SectionsPerDoc: 3, entriesPerSection: 4);
+            var newEntry = new Entry(999, "new entry content");
+
+            // Assert
+            Assert.Throws<Exception>(() =>
+            {
+                // Act
+                dbMock.CreateEntryinSection(documentId: 0, sectionId: 10, newEntry);
+            });
+        }
+
+        [Fact]
+        public void UpdateEntryinSection_ValidData_EntryUpdated()
+        {
+            // Arrange
+            var dbMock = new DBMock(docNumber: 2, SectionsPerDoc: 3, entriesPerSection: 4);
+            var newEntry = new Entry(999, "updated entry content");
+
+            // Act
+            dbMock.UpdateEntryinSection(documentId: 0, sectionId: 1, oldEntryId: 2, newEntry);
+            var entry = dbMock.ReadEntryinSection(documentId: 0, entryId: 999);
+
+            // Assert
+            Assert.Equal(newEntry, entry);
+        }
+
+        [Fact]
+        public void UpdateEntryinSection_InvalidSection_ThrowsException()
+        {
+            // Arrange
+            var dbMock = new DBMock(docNumber: 2, SectionsPerDoc: 3, entriesPerSection: 4);
+            var newEntry = new Entry(999, "updated entry content");
+
+            // Assert
+            Assert.Throws<Exception>(() =>
+            {
+                // Act
+                dbMock.UpdateEntryinSection(documentId: 0, sectionId: 10, oldEntryId: 2, newEntry);
+            });
+        }
+
+        [Fact]
+        public void UpdateEntryinSection_InvalidEntry_ThrowsException()
+        {
+            // Arrange
+            var dbMock = new DBMock(docNumber: 2, SectionsPerDoc: 3, entriesPerSection: 4);
+            var newEntry = new Entry(999, "updated entry content");
+
+            // Assert
+            Assert.Throws<Exception>(() =>
+            {
+                // Act
+                dbMock.UpdateEntryinSection(documentId: 0, sectionId: 1, oldEntryId: 10, newEntry);
+            });
+        }
+
+        [Fact]
+        public void DeleteEntryinSection_ValidData_EntryDeleted()
+        {
+            // Arrange
+            var dbMock = new DBMock(docNumber: 2, SectionsPerDoc: 3, entriesPerSection: 4);
+
+            // Act
+            dbMock.DeleteEntryinSection(documentId: 0, sectionId: 1, entryId: 2);
+            var entry = dbMock.ReadEntryinSection(documentId: 0, entryId: 2);
+
+            // Assert
+            Assert.Null(entry);
+        }
+
+        [Fact]
+        public void DeleteEntryinSection_InvalidSection_ThrowsException()
+        {
+            // Arrange
+            var dbMock = new DBMock(docNumber: 2, SectionsPerDoc: 3, entriesPerSection: 4);
+
+            // Assert
+            Assert.Throws<Exception>(() =>
+            {
+                // Act
+                dbMock.DeleteEntryinSection(documentId: 0, sectionId: 10, entryId: 2);
+            });
+        }
+
+        [Fact]
+        public void DeleteEntryinSection_InvalidEntry_ThrowsException()
+        {
+            // Arrange
+            var dbMock = new DBMock(docNumber: 2, SectionsPerDoc: 3, entriesPerSection: 4);
+
+            // Assert
+            Assert.Throws<Exception>(() =>
+            {
+                // Act
+                dbMock.DeleteEntryinSection(documentId: 0, sectionId: 1, entryId: 10);
+            });
+        }
+    }
+}
---
+++ Domain.Entities/Mocks/DBMock.cs
@@ -0,0 +1,140 @@
+﻿using Domain.Entities.DataObjects.DocumentComposite;
+using Domain.Entities.DataObjects.EntryComposite;
+using Domain.Entities.DataObjects;
+using Domain.Entities.PersistenceServices.EntryPersistenceServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Mocks
+{
+    public class DBMock : ISectionEntryInSectionCRUDPersistenceService
+    {
+        public Dictionary<int, Document> Documents = new Dictionary<int, Document>();
+        public Dictionary<(int, int), SectionComposite> Sections = new Dictionary<(int, int), SectionComposite>();
+        public Dictionary<(int, int), Entry> Entries = new Dictionary<(int, int), Entry>();
+
+
+
+        public DBMock(int docNumber, int SectionsPerDoc, int entriesPerSection)
+        {
+            AddDocuments(docNumber);
+            AddNSectionstoDocuments(SectionsPerDoc);
+            AddNEntriesToAllSections(entriesPerSection);
+        }
+        internal void AddDocuments(int docNumber)
+        {
+            for (int i = 0; i < docNumber; i++)
+            {
+                Document temp = new Document(i, "testdoc " + i.ToString(), new List<SectionComponent>(), new LanguagesComponent(3));
+                Documents.Add(i, temp);
+            }
+        }
+        internal void AddNSectionstoDocuments(int listLenght)
+        {
+            foreach (Document tempDoc in Documents.Values)
+            {
+                for (int i = 1; i < listLenght + 1; i++)
+                {
+                    SectionComposite tempSection = new SectionComposite("section test" + i.ToString(), i, tempDoc.GetLanguageComponent(), tempDoc.SystemId);
+                    tempDoc.AddSection(tempSection);
+                    Sections.Add((tempDoc.SystemId, tempSection.SectionIdDoc), tempSection);
+                }
+            }
+        }
+        internal void AddNEntriesToAllSections(int numberOfEntries)
+        {
+            foreach (SectionComposite tempSection in Sections.Values) 
+            {
+                for (int i = 0; i < numberOfEntries; i++)
+                {
+                    // Generate a unique key for the entry based on the section and entry index
+                    var random = new Random();
+                    int id = random.Next(1, int.MaxValue);
+                    var entryKey = (tempSection.SourceDocument, id);
+
+                    // Check if the entryKey already exists in the dictionary
+                    while (Entries.ContainsKey(entryKey))
+                    {
+                        // Generate a new key if it already exists
+                        int newId = random.Next(1, int.MaxValue);
+                        entryKey = (tempSection.SourceDocument, newId); 
+                    }
+                    Entry tempEntry = new Entry(id, "some entry content " + i.ToString());
+
+                    Entries.Add(entryKey, tempEntry);
+                }
+
+            }
+        }
+        public Dictionary<(int, int), SectionComposite> GetSections()
+        {
+            return Sections;
+        }
+        public SectionComposite GetSectionComposite(int documentId, int sectionId)
+        {
+            if (Documents.ContainsKey(documentId) && Sections.ContainsKey((documentId, sectionId)))
+            {
+                return Sections[(documentId, sectionId)];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Entry ReadEntryinSection(int documentId, int entryId)
+        {
+            if (Documents.ContainsKey(documentId) && Entries.ContainsKey((documentId, entryId)))
+            {
+                return Entries[(documentId, entryId)];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void CreateEntryinSection(int documentId, int sectionId, Entry newEntry)
+        {
+            if (Sections[(documentId, sectionId)] != null)
+            {
+                //here we are not compeling with the Business logic for the use case as we are not ensuring that 
+                //the section list object is properly updated to contain the new entry, although we are adding it to the entries list. I need to adress this later as it might cause discrepancies in the DB behaviour
+                Entries.Add((documentId, newEntry.Id), newEntry);
+            }
+            else
+            {
+                throw new Exception("no section to add entry to");
+            }
+        }
+
+        public void UpdateEntryinSection(int documentId, int sectionId, int oldEntryId, Entry newEntry)
+        {
+            if (Sections[(documentId, sectionId)] != null && Entries[(documentId, oldEntryId)] != null)
+            {
+                DeleteEntryinSection(documentId, sectionId, oldEntryId);
+                Entries.Add((documentId, newEntry.Id), newEntry);
+            }
+            else
+            {
+                throw new Exception("no entry in section to update");
+            }
+        }
+
+        public void DeleteEntryinSection(int documentId, int sectionId, int entryId)
+        {
+            if (Sections[(documentId, sectionId)] != null && Entries[(documentId, entryId)] != null)
+            {
+                Entries.Remove((documentId, entryId));
+            }
+            else
+            {
+                throw new Exception("no entry in section to Deleate");
+            }
+        }
+    }
+
+}
+
